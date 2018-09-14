35053da1df2f56293b3fe38d03b24590d020fa8f
PeterMacDonnell <petermacdonnell@berkeley.edu>
2018-09-13 22:59:31 -0700

    initial submission, render issues

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('The Big Boi');
+INSERT INTO burgers (burger_name) VALUES ('Thick MEATS');
+INSERT INTO burgers (burger_name, devoured) VALUES ('A Thick burger', true);
